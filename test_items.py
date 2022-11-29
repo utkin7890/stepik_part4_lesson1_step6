4e507692ffdd81bf2b8b9d851936bf5a77b0bd2c
utkin-ivan <89164117260@mail.ru>
2022-11-29 10:52:07 +0300

    added files conftest requirements test_items

--- test_items.py
+++ test_items.py
@@ -5,9 +5,9 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
-    time.sleep(50)
+    browser.find_element(By.XPATH, "//button[@value = 'Добавить в корзину']")
+    time.sleep(5)
