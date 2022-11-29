026c510a6e39649d678784d10a5a6125ff9d6c70
utkin-ivan <89164117260@mail.ru>
2022-11-29 11:08:06 +0300

    edited test code

--- test_items.py
+++ test_items.py
@@ -1,13 +1,9 @@
-import pytest
 from selenium.webdriver.common.by import By
-import selenium
 import time
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element(By.XPATH, "//button[@value = 'Добавить в корзину']")
+    browser.find_element(By.XPATH, "//form[@id = 'add_to_basket_form']//button")
     time.sleep(5)
