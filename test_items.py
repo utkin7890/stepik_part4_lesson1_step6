42771ec96b95fd86c04f2de18a24f8a67f49ffc7
89164 <poipoipoi123!@#>
2022-11-30 22:19:09 +0300

    edit time.sleep(5)

--- test_items.py
+++ test_items.py
@@ -9,3 +9,6 @@ def test_should_be_button_add_to_cart(browser):
     time.sleep(5)
     button_add_to_cart = browser.find_element(By.XPATH, '//form[@id = "add_to_basket_form"]//button')
     assert button_add_to_cart.is_enabled() , 'The button is not available!'
+
+
+# commit-1
