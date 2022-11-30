30b2fade45be0845ab922d4f3ad8d30acc08b85e
89164 <poipoipoi123!@#>
2022-11-30 22:14:21 +0300

    edit time.sleep(5)

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_should_be_button_add_to_cart(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(5)
     button_add_to_cart = browser.find_element(By.XPATH, '//form[@id = "add_to_basket_form"]//button')
     assert button_add_to_cart.is_enabled() , 'The button is not available!'
