4978529bbb35f525bce1b347e978547f4c86ab6c
89164 <poipoipoi123!@#>
2022-12-01 00:12:37 +0300

    added method is_element_present in class BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -9,6 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_link()
+    page.is_element_present()
     time.sleep(3)
 
 
